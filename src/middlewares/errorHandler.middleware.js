f8a0641e508d97039086a725348b3a5ce34e43b6
Vu Truong Anh <truonganhvu205@gmail.com>
2025-10-10 15:33:39 +0700

    v1

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,13 +1,10 @@
 function errorHandler(err, req, res, next) {
     const status = err.status || 500
     const message = err.message || 'Internal Server Error'
-    const isDev = process.env.NODE_ENV === 'development'
 
     res.status(status).render('error', {
         title: `${status}`,
         message,
-        stack: isDev ? err.stack : null,
-        isDev,
     })
 }
 
