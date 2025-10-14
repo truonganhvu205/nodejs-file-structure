48ecc230a3ff5ed0c911ef550aa2e90df19b2039
Vu Truong Anh <truonganhvu205@gmail.com>
2025-10-14 10:12:32 +0700

    v1.2 - connect postgresql

--- src/config/env.js
+++ src/config/env.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
 const env = {
-    serverPort: process.env.PORT,
-    mongoDbUri: process.env.MONGODB_URI,
-    postgreUri: process.env.POSTGRE_URI,
+    serverPort: process.env.PORT || 3000,
+    mongoDbUri: process.env.MONGODB_URI || '',
+    postgreUri: process.env.POSTGRE_URI || '',
 }
 
 module.exports = env
