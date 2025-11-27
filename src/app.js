ab2246d68881a7a92ddaed9a2ecfc0998caa67a9
Vu Truong Anh <truonganhvu205@gmail.com>
2025-11-27 16:40:40 +0700

    patch

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const {engine} = require('express-handlebars')
 const path = require('path')
 const setupRoutes = require('./app/routes')
 const methodOverride = require('method-override')
-const {notFoundHandler, errorHandler} = require('./app/middlewares')
+const {defaultLayout, notFoundHandler, errorHandler} = require('./app/middlewares')
 const {connectPostgreSql, connectMongoDb, connectRedis} = require('./app/databases')
 
 const app = express()
@@ -32,10 +32,10 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(methodOverride('_method'))
 
+app.use(defaultLayout)
+
 app.engine('.hbs', engine({
     extname: '.hbs',
-    defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'resources', 'views', 'layouts'),
 }))
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'resources', 'views'))
